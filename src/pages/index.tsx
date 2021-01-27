81f54f913360613130bfda89ddff45476c6c568d
florian <duceptflorian53@gmail.com>
2021-01-27 21:26:45 +0100

    test github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/header";
 const App: React.FC = () => {
 	return (
 		<>
-			<Header />
+			<Header />I am sorry for css
 			<h1>Welcome to mastering next js this is a test ! </h1>
 		</>
 	);
